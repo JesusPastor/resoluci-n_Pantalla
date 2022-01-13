33c00a7e16f88cefd4f54210f7855552b5790cec
Jesús <jesuspastorgonzalez@outlook.com>
2022-01-13 20:50:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let width = screen.width;
+let height = screen.height;
+var concatenacion = "El ancho de la pantalla es "+width+"px"+" y el alto es "+height+"px";
+var resultado = document.getElementById("resultado");
+resultado.innerHTML = concatenacion;
